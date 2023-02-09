731e1123ad2390c7790ccc25911e24a458751885
rezaa <rezaageng5@gmail.com>
2023-02-09 23:02:02 +0700

    feat(navbar): :sparkles: add navbar animations

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -2,62 +2,169 @@
 
 import Link from 'next/link'
 import { usePathname } from 'next/navigation'
-import { motion } from 'framer-motion'
+import { motion, useAnimationControls, useScroll } from 'framer-motion'
 import NavbarUnderline from './NavbarUnderline'
+import { useEffect, useState } from 'react'
+import { useMediaQuery } from 'react-responsive'
 
 const Navbar = (): JSX.Element => {
+  const [isInitial, setIsInitial] = useState(true)
+  const [isOpen, setIsOpen] = useState(false)
+  const [isSm, setIsSm] = useState(true)
+  const [isVisible, setIsVisible] = useState(true)
+  const [previousScrollPosition, setPreviousScrollPosition] = useState(0)
+
+  const smallScreen = useMediaQuery({ query: '(min-width: 640px)' })
   const pathName = usePathname()
 
+  const navControl = useAnimationControls()
+  const menuControl = useAnimationControls()
+
+  const { scrollYProgress } = useScroll()
+
+  useEffect(() => {
+    scrollYProgress.on('change', (progress) => {
+      if (progress > previousScrollPosition + 0.1) {
+        setIsVisible(false)
+        setPreviousScrollPosition(scrollYProgress.get())
+      } else if (progress < previousScrollPosition - 0.1) {
+        setIsVisible(true)
+        setPreviousScrollPosition(scrollYProgress.get())
+      }
+    })
+  }, [previousScrollPosition, scrollYProgress])
+
+  useEffect(() => {
+    if (isVisible) {
+      void navControl.start({
+        y: 0,
+        transition: {
+          type: 'spring',
+          stiffness: 300,
+          damping: 50,
+          restDelta: 0.001
+        }
+      })
+    } else {
+      void navControl.start({
+        y: -100,
+        transition: {
+          type: 'spring',
+          stiffness: 300,
+          damping: 100,
+          restDelta: 0.001
+        }
+      })
+    }
+  }, [isVisible, navControl])
+
+  useEffect(() => {
+    if (isOpen && !isSm) {
+      void menuControl.start({
+        x: [650, 0],
+        transition: {
+          type: 'spring',
+          stiffness: 300,
+          damping: 50,
+          restDelta: 0.001
+        }
+      })
+    }
+    if (!isOpen && !isSm) {
+      void menuControl.start({
+        x: [0, 650],
+        transition: {
+          type: 'spring',
+          stiffness: 300,
+          damping: 100,
+          restDelta: 0.001
+        }
+      })
+    }
+    if (isSm) {
+      void menuControl.start({
+        x: 0
+      })
+    }
+  }, [isOpen, isSm, menuControl])
+
+  useEffect(() => {
+    setIsSm(smallScreen)
+    if (smallScreen) {
+      setIsInitial(false)
+    }
+  }, [smallScreen])
+
   return (
-    <nav className="flex w-full justify-between bg-primary p-4">
-      <h1>rezaa</h1>
-      <ul className="fixed top-0 left-0 flex h-screen w-full flex-col items-center justify-center gap-5 bg-secondary-800 text-2xl font-bold sm:static sm:h-auto sm:w-auto sm:flex-row sm:bg-transparent sm:text-base sm:font-normal">
-        <li className="relative">
-          {pathName === '/' ? <NavbarUnderline /> : null}
-          <Link
-            className={`${
-              pathName === '/' ? 'text-accent-1' : ''
-            } after:contents hover:animate-pulse`}
-            href="/"
+    <motion.nav
+      animate={navControl}
+      className="fixed top-0 left-0 flex w-full justify-between bg-primary p-4"
+    >
+      <div className="z-10 flex w-full justify-between text-accent-1 sm:block sm:w-auto sm:justify-start">
+        <h1>rezaa</h1>
+        {!isSm ? (
+          <button
+            onClick={() => {
+              setIsOpen((state) => !state)
+              setIsInitial(false)
+            }}
           >
-            Home
-          </Link>
-        </li>
-        <motion.li className="relative">
-          {pathName === '/projects' ? <NavbarUnderline /> : null}
-          <Link
-            className={`${
-              pathName === '/projects' ? 'text-accent-1' : ''
-            } hover:animate-pulse`}
-            href="/projects"
-          >
-            Projects
-          </Link>
-        </motion.li>
-        <motion.li className="relative">
-          {pathName === '/another-side' ? <NavbarUnderline /> : null}
-          <Link
-            className={`${
-              pathName === '/another-side' ? 'text-accent-1' : ''
-            } hover:animate-pulse`}
-            href="/another-side"
-          >
-            Another Side
-          </Link>
-        </motion.li>
-        <li className="relative">
-          {pathName === '/about' ? <NavbarUnderline /> : null}
-          <Link
-            className={`${
-              pathName === '/about' ? 'text-accent-1' : ''
-            } hover:animate-pulse`}
-            href="/about"
-          >
-            About
-          </Link>
-        </li>
-      </ul>
-    </nav>
+            menu
+          </button>
+        ) : null}
+      </div>
+      {!isInitial ? (
+        <motion.ul
+          animate={menuControl}
+          className="fixed top-0 left-0 flex h-screen w-full flex-col items-center justify-center gap-5 bg-secondary-800 text-2xl font-bold sm:static sm:h-auto sm:w-auto sm:flex-row sm:bg-transparent sm:text-base sm:font-normal"
+        >
+          <li className="relative">
+            {pathName === '/' ? <NavbarUnderline /> : null}
+            <Link
+              className={`${
+                pathName === '/' ? 'text-accent-1' : ''
+              } after:contents hover:animate-pulse`}
+              href="/"
+            >
+              Home
+            </Link>
+          </li>
+          <motion.li className="relative">
+            {pathName === '/projects' ? <NavbarUnderline /> : null}
+            <Link
+              className={`${
+                pathName === '/projects' ? 'text-accent-1' : ''
+              } hover:animate-pulse`}
+              href="/projects"
+            >
+              Projects
+            </Link>
+          </motion.li>
+          <motion.li className="relative">
+            {pathName === '/another-side' ? <NavbarUnderline /> : null}
+            <Link
+              className={`${
+                pathName === '/another-side' ? 'text-accent-1' : ''
+              } hover:animate-pulse`}
+              href="/another-side"
+            >
+              Another Side
+            </Link>
+          </motion.li>
+          <li className="relative">
+            {pathName === '/about' ? <NavbarUnderline /> : null}
+            <Link
+              className={`${
+                pathName === '/about' ? 'text-accent-1' : ''
+              } hover:animate-pulse`}
+              href="/about"
+            >
+              About
+            </Link>
+          </li>
+        </motion.ul>
+      ) : null}
+    </motion.nav>
   )
 }
 
--- components/NavbarUnderline.tsx
+++ components/NavbarUnderline.tsx
@@ -3,6 +3,7 @@ import { motion } from 'framer-motion'
 const NavbarUnderline = (): JSX.Element => {
   return (
     <motion.div
+      data-testid="navbar-underline"
       layoutId="navbar-underline"
       className="absolute -bottom-1 left-0 h-1 w-full rounded-full bg-accent-1 sm:h-[0.10rem]"
       transition={{
