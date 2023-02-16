6fe01a1ff6a3ce9cb67b6a833312278a012493a8
rezaa <rezaageng5@gmail.com>
2023-02-16 17:50:20 +0700

    feat(sidebar): :sparkles: activate hover animation

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -6,7 +6,7 @@ const Sidebar = (): JSX.Element => {
       data-testid="sidebar"
       className="fixed bottom-0 right-8 hidden flex-col items-center gap-4 sm:flex"
     >
-      <Contacts iconSize={20} className="flex flex-col gap-4" />
+      <Contacts iconSize={20} className="flex flex-col gap-4" animate />
       <div data-testid="line" className="h-24 w-[1px] rounded-full bg-white" />
     </aside>
   )
