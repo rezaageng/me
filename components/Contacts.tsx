2fe33e35123c86f87c3dd8e5169fea14bc97e41b
rezaa <rezaageng5@gmail.com>
2023-02-17 22:22:46 +0700

    feat(contacts): :sparkles: add hover animation

--- components/Contacts.tsx
+++ components/Contacts.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { type ContactsProps } from '@/@types'
+import { type Social, type ContactsProps } from '@/@types'
 import {
   FiGithub,
   FiInstagram,
@@ -14,60 +14,58 @@ import {
   type VariantLabels
 } from 'framer-motion'
 import useFramerStore from '@/store/framerStore'
+import { v4 as uuidv4 } from 'uuid'
 
 const Contacts = ({
   className,
-  iconSize,
+  iconSize = 16,
   animate = false
 }: ContactsProps): JSX.Element => {
   const { transition } = useFramerStore((state) => state)
 
   const hoverAnimation: TargetAndTransition | VariantLabels = {
-    y: [0, -5],
-    scale: [1, 1.2],
+    y: [0, -10],
+    scale: [1, 1.5],
+    marginTop: [0, 16],
     color: ['#fff', '#259D97'],
     transition
   }
 
+  const socials: Social[] = [
+    {
+      icon: <FiGithub data-testid="contact-icon" size={iconSize} />,
+      link: 'https://github.com/rezaageng'
+    },
+    {
+      icon: <FiTwitter data-testid="contact-icon" size={iconSize} />,
+      link: 'https://twitter.com/rezaageng_'
+    },
+    {
+      icon: <FiInstagram data-testid="contact-icon" size={iconSize} />,
+      link: 'https://www.instagram.com/rezaageng_/'
+    },
+    {
+      icon: <FiLinkedin data-testid="contact-icon" size={iconSize} />,
+      link: 'https://www.linkedin.com/in/rezaageng/'
+    },
+    {
+      icon: <FiMail data-testid="contact-icon" size={iconSize} />,
+      link: 'mailto:waiting@gmail.com'
+    }
+  ]
+
   return (
     <motion.ul className={className}>
-      <motion.li whileHover={animate ? hoverAnimation : undefined}>
-        <a href="https://github.com/rezaageng" target="_blank" rel="noreferrer">
-          <FiGithub data-testid="contact-icon" size={iconSize} />
-        </a>
-      </motion.li>
-      <motion.li whileHover={animate ? hoverAnimation : undefined}>
-        <a
-          href="https://twitter.com/rezaageng_"
-          target="_blank"
-          rel="noreferrer"
+      {socials.map(({ icon, link }, index) => (
+        <motion.li
+          key={uuidv4()}
+          whileHover={animate ? hoverAnimation : undefined}
         >
-          <FiTwitter data-testid="contact-icon" size={iconSize} />
-        </a>
-      </motion.li>
-      <motion.li whileHover={animate ? hoverAnimation : undefined}>
-        <a
-          href="https://www.instagram.com/rezaageng_/"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <FiInstagram data-testid="contact-icon" size={iconSize} />
-        </a>
-      </motion.li>
-      <motion.li whileHover={animate ? hoverAnimation : undefined}>
-        <a
-          href="https://www.linkedin.com/in/rezaageng/"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <FiLinkedin data-testid="contact-icon" size={iconSize} />
-        </a>
-      </motion.li>
-      <motion.li whileHover={animate ? hoverAnimation : undefined}>
-        <a href="mailto:waiting@gmail.com" target="_blank" rel="noreferrer">
-          <FiMail data-testid="contact-icon" size={iconSize} />
-        </a>
-      </motion.li>
+          <a href={link} target="_blank" rel="noreferrer">
+            {icon}
+          </a>
+        </motion.li>
+      ))}
     </motion.ul>
   )
 }
