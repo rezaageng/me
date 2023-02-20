491e1241a01c86f2ebb7983d8b53072b3073879e
rezaa <rezaageng5@gmail.com>
2023-02-20 11:41:19 +0700

    feat(home): :construction: wip home main

---
+++ app/components/HomeMain.tsx
@@ -0,0 +1,23 @@
+import { type HomeResponse } from '@/@types'
+import Image from 'next/image'
+
+const HomeMain = ({ data }: HomeResponse): JSX.Element => {
+  return (
+    <div
+      data-testid="home-main"
+      className="flex min-h-screen w-full flex-col justify-center px-8 py-4"
+    >
+      <div>
+        <Image
+          data-testid="main-character"
+          src="/assets/images/mc.png"
+          width={2738}
+          height={4000}
+          alt="rezaa"
+        />
+      </div>
+      <h1>{data.attributes.title}</h1>
+    </div>
+  )
+}
+export default HomeMain
--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,28 @@
-const Home = (): JSX.Element => {
+import { type HomeResponse } from '@/@types'
+import HomeMain from './components/HomeMain'
+
+const getData = async (): Promise<HomeResponse> => {
+  const res = await fetch(`${process.env.API_URL}/home`, {
+    method: 'get',
+    headers: {
+      authorization: `Bearer ${process.env.API_KEY}`
+    }
+  })
+
+  if (!res.ok) {
+    throw new Error('error')
+  }
+
+  return await res.json()
+}
+
+const Home = async (): Promise<JSX.Element> => {
+  const { data } = await getData()
+
   return (
-    <div className="grid min-h-screen w-full place-content-center text-4xl font-bold">
-      Hello
-    </div>
+    <>
+      <HomeMain data={data} />
+    </>
   )
 }
 export default Home
