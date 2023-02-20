758614d67b9008c416465fe6b809bf50497289bc
rezaa <rezaageng5@gmail.com>
2023-02-20 11:37:48 +0700

    test(home): :fire: remove test

--- __test__/pages/home.test.tsx
+++
@@ -1,8 +0,0 @@
-import Home from '@/app/page'
-import { render, screen } from '@testing-library/react'
-
-test('render text', () => {
-  render(<Home />)
-  const text = screen.getByText(/Hello/i)
-  expect(text).toBeInTheDocument()
-})
