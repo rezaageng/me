b1d0ff3ef38e910f17d57da014608e4f86c960f3
rezaa <rezaageng5@gmail.com>
2023-06-25 19:00:03 +0700

    feat(app): :sparkles: setup react query

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,8 @@ import Navbar from '@/components/Navbar'
 import './globals.css'
 import { type Metadata } from 'next'
 import Sidebar from '@/components/Sidebar'
+import QueryProvider from './query-provider'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 
 const poppins = Poppins({
   weight: ['400', '700'],
@@ -33,13 +35,16 @@ const RootLayout = ({
     <html lang="en" className={`${poppins.variable} ${hackNf.variable}`}>
       <head />
       <body className="bg-primary font-sans text-white scrollbar-thin scrollbar-track-secondary-900 scrollbar-thumb-accent-1 scrollbar-thumb-rounded">
-        <header>
-          <Navbar />
-        </header>
-        <main className="m-auto mt-16 max-w-7xl">{children}</main>
-        <aside>
-          <Sidebar />
-        </aside>
+        <QueryProvider>
+          <ReactQueryDevtools initialIsOpen={false} />
+          <header>
+            <Navbar />
+          </header>
+          <main className="m-auto mt-16 max-w-7xl">{children}</main>
+          <aside>
+            <Sidebar />
+          </aside>
+        </QueryProvider>
       </body>
     </html>
   )
