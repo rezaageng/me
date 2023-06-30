d5f73d0fd6b7e79f02a9c0a180fc93c5013cdee2
rezaa <rezaageng5@gmail.com>
2023-06-30 21:52:04 +0700

    test(terminal): :test_tube: add form test

--- src/__tests__/components/TerminalPrompt.test.tsx
+++ src/__tests__/components/TerminalPrompt.test.tsx
@@ -1,42 +1,54 @@
-import Terminal from '@/components/terminal/Terminal'
+import TerminalPrompt from '@/components/terminal/TerminalPrompt'
 import { fireEvent, render, screen } from '@testing-library/react'
 
 test('should render terminal prompt', () => {
-  render(<Terminal />)
+  render(<TerminalPrompt index={0} />)
   const terminalPrompt = screen.getByTestId('terminal-prompt')
 
   expect(terminalPrompt).toBeInTheDocument()
 })
 
 test('should render terminal next version', () => {
-  render(<Terminal />)
+  render(<TerminalPrompt index={0} />)
   const terminalNextVersion = screen.getByTestId('terminal-next-ver')
 
   expect(terminalNextVersion).toBeInTheDocument()
 })
 
 test('should render terminal next version with correct text', () => {
-  render(<Terminal />)
+  render(<TerminalPrompt index={0} />)
   const terminalNextVersion = screen.getByTestId('terminal-next-ver')
 
   expect(terminalNextVersion).toHaveTextContent('v13.4.5')
 })
 
 test('should render terminal prompt with correct text', () => {
-  render(<Terminal />)
+  render(<TerminalPrompt index={0} />)
   const terminalPrompt = screen.getByTestId('terminal-prompt')
 
   expect(terminalPrompt).toHaveTextContent('rezaa@xe ~')
 })
 
 test('should render enabled input', () => {
-  render(<Terminal />)
+  render(<TerminalPrompt index={0} />)
   const terminalInput = screen.getByTestId('terminal-input')
 
   expect(terminalInput).not.toHaveAttribute('disabled')
 
   const inputText = 'test input'
   fireEvent.change(terminalInput, { target: { value: inputText } })
+
+  expect(terminalInput).toHaveValue(inputText)
+})
+
+test('should submit form', () => {
+  render(<TerminalPrompt index={0} />)
+  const terminalInput = screen.getByTestId('terminal-input')
+
+  const inputText = 'xe'
+  fireEvent.change(terminalInput, { target: { value: inputText } })
+
+  fireEvent.submit(terminalInput)
 
   expect(terminalInput).toHaveValue(inputText)
 })
