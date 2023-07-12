6bc012f993d6089502c56cfe3d72714b8f24cbb8
rezaa <rezaageng5@gmail.com>
2023-07-12 16:13:13 +0700

    test(mock): :clown_face: add contact mock

--- src/__mocks__/handlers.ts
+++ src/__mocks__/handlers.ts
@@ -1,3 +1,4 @@
+import { type ContactsResponse } from '@/@types/contacts'
 import { type EducationsResponse } from '@/@types/educations'
 import { type ExperiencesResponse } from '@/@types/experiences'
 import { type HomeResponse } from '@/@types/home'
@@ -887,6 +888,134 @@ export const handlers = [
               pageSize: 25,
               pageCount: 1,
               total: 2
+            }
+          }
+        })
+      )
+  ),
+  rest.get(
+    `${process.env.API_KEY}/api/contacts`,
+    async (_req, res, ctx) =>
+      await res(
+        ctx.status(200),
+        ctx.json<ContactsResponse>({
+          data: [
+            {
+              id: 1,
+              attributes: {
+                name: 'GitHub',
+                link: 'https://github.com/rezaageng',
+                createdAt: '2023-07-12T08:48:56.052Z',
+                updatedAt: '2023-07-12T08:48:56.052Z'
+              }
+            },
+            {
+              id: 2,
+              attributes: {
+                name: 'Twitter',
+                link: 'https://twitter.com/rezaageng_',
+                createdAt: '2023-07-12T08:49:58.704Z',
+                updatedAt: '2023-07-12T08:49:58.704Z'
+              }
+            },
+            {
+              id: 3,
+              attributes: {
+                name: 'Instagram',
+                link: 'https://www.instagram.com/rezaageng_/',
+                createdAt: '2023-07-12T08:51:06.458Z',
+                updatedAt: '2023-07-12T08:51:06.458Z'
+              }
+            },
+            {
+              id: 4,
+              attributes: {
+                name: 'LinkedIn',
+                link: 'https://www.linkedin.com/in/rezaageng/',
+                createdAt: '2023-07-12T08:51:56.319Z',
+                updatedAt: '2023-07-12T08:51:56.319Z'
+              }
+            },
+            {
+              id: 5,
+              attributes: {
+                name: 'rezaageng47@gmail.com',
+                link: 'mailto:rezaageng47@gmail.com',
+                createdAt: '2023-07-12T08:54:11.453Z',
+                updatedAt: '2023-07-12T08:54:11.453Z'
+              }
+            }
+          ],
+          meta: {
+            pagination: {
+              page: 1,
+              pageSize: 25,
+              pageCount: 1,
+              total: 5
+            }
+          }
+        })
+      )
+  ),
+  rest.get(
+    '/api/contacts',
+    async (_req, res, ctx) =>
+      await res(
+        ctx.status(200),
+        ctx.json<ContactsResponse>({
+          data: [
+            {
+              id: 1,
+              attributes: {
+                name: 'GitHub',
+                link: 'https://github.com/rezaageng',
+                createdAt: '2023-07-12T08:48:56.052Z',
+                updatedAt: '2023-07-12T08:48:56.052Z'
+              }
+            },
+            {
+              id: 2,
+              attributes: {
+                name: 'Twitter',
+                link: 'https://twitter.com/rezaageng_',
+                createdAt: '2023-07-12T08:49:58.704Z',
+                updatedAt: '2023-07-12T08:49:58.704Z'
+              }
+            },
+            {
+              id: 3,
+              attributes: {
+                name: 'Instagram',
+                link: 'https://www.instagram.com/rezaageng_/',
+                createdAt: '2023-07-12T08:51:06.458Z',
+                updatedAt: '2023-07-12T08:51:06.458Z'
+              }
+            },
+            {
+              id: 4,
+              attributes: {
+                name: 'LinkedIn',
+                link: 'https://www.linkedin.com/in/rezaageng/',
+                createdAt: '2023-07-12T08:51:56.319Z',
+                updatedAt: '2023-07-12T08:51:56.319Z'
+              }
+            },
+            {
+              id: 5,
+              attributes: {
+                name: 'rezaageng47@gmail.com',
+                link: 'mailto:rezaageng47@gmail.com',
+                createdAt: '2023-07-12T08:54:11.453Z',
+                updatedAt: '2023-07-12T08:54:11.453Z'
+              }
+            }
+          ],
+          meta: {
+            pagination: {
+              page: 1,
+              pageSize: 25,
+              pageCount: 1,
+              total: 5
             }
           }
         })
