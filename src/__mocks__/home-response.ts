22095baf1780083d29e0a5d91fd8f904e7413c16
rezaa <rezaageng5@gmail.com>
2023-07-16 19:20:03 +0700

    test(mock): :clown_face: add home response mock

---
+++ src/__mocks__/home-response.ts
@@ -0,0 +1,20 @@
+import { type HomeResponse } from '@/@types/home'
+
+const homeRes: HomeResponse = {
+  data: {
+    id: 1,
+    attributes: {
+      title: "Hello, I'm rezaa.",
+      subtitle:
+        'Front-end developer and Informatics Engineering student at Pasundan University',
+      createdAt: '2023-02-06T07:37:17.769Z',
+      updatedAt: '2023-06-25T06:35:18.416Z',
+      publishedAt: '2023-02-06T07:44:18.682Z',
+      anotherSide: 'i like games, anime, manga, and manhwa',
+      summary: 'I like to code especially in front end dev heheheheh'
+    }
+  },
+  meta: {}
+}
+
+export default homeRes
