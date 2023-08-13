8f1c5d59234910a28df29c69abbad3353bdcaace
rezaa <rezaageng5@gmail.com>
2023-08-13 15:16:01 +0700

    test(home): :test_tube: update tests

--- src/__tests__/components/home/HomeStats.test.tsx
+++ src/__tests__/components/home/HomeStats.test.tsx
@@ -1,45 +1,11 @@
 /* eslint-disable jest/no-mocks-import */
-import HomeStats from '@/components/home/HomeStats'
-import HomeWakaWeek from '@/components/home/stats/HomeWakaWeek'
-import { render, screen } from '@testing-library/react'
-import '@/__mocks__/intersectionObserverMock'
 
-test('should render skills', async () => {
-  render(await HomeStats())
+test.todo('should render skills')
 
-  const skills = screen.getByTestId('bento-skills')
+test.todo('should render github stats')
 
-  expect(skills).toBeInTheDocument()
-})
+test.todo('should render wakatime all time')
 
-test('should render github stats', async () => {
-  render(await HomeStats())
+test.todo('should render wakatime last 7 days')
 
-  const githubStats = screen.getByTestId('bento-github')
-
-  expect(githubStats).toBeInTheDocument()
-})
-
-test('should render wakatime all time', async () => {
-  render(await HomeStats())
-
-  const wakatimeAllTime = screen.getByTestId('bento-waka-all')
-
-  expect(wakatimeAllTime).toBeInTheDocument()
-})
-
-test('should render wakatime last 7 days', async () => {
-  render(<HomeWakaWeek />)
-
-  const wakaWeek = screen.getByTestId('bento-waka-week')
-
-  expect(wakaWeek).toBeInTheDocument()
-})
-
-test('should render leetcode stats', async () => {
-  render(await HomeStats())
-
-  const leetcodeStats = screen.getByTestId('bento-leet')
-
-  expect(leetcodeStats).toBeInTheDocument()
-})
+test.todo('should render leetcode stats')
--- src/__tests__/components/home/stats/HomeGithubStats.test.tsx
+++ src/__tests__/components/home/stats/HomeGithubStats.test.tsx
@@ -1,8 +1,10 @@
+/* eslint-disable jest/no-mocks-import */
+import { gitHubStats } from '@/__mocks__/github'
 import HomeGithubStats from '@/components/home/stats/HomeGithubStats'
 import { render, screen } from '@testing-library/react'
 
 test('should render title', () => {
-  render(<HomeGithubStats />)
+  render(<HomeGithubStats data={gitHubStats} />)
 
   const title = screen.getByText('GitHub Stats')
 
@@ -10,41 +12,51 @@ test('should render title', () => {
 })
 
 test('should render public repositories count', () => {
-  render(<HomeGithubStats />)
+  render(<HomeGithubStats data={gitHubStats} />)
 
   const publicRepos = screen.getByTestId('public-repos')
 
-  expect(publicRepos.innerHTML).toBe('2 Public Repositories')
+  expect(publicRepos.innerHTML).toBe('29 Public Repositories')
 })
 
 test('should render stars earned count', () => {
-  render(<HomeGithubStats />)
+  render(<HomeGithubStats data={gitHubStats} />)
 
   const stars = screen.getByTestId('stars')
 
-  expect(stars.innerHTML).toBe('10 Stars Earned')
+  expect(stars.innerHTML).toBe('11&nbsp;Stars Earned')
 })
 
 test('should render total commits', () => {
-  render(<HomeGithubStats />)
+  render(<HomeGithubStats data={gitHubStats} />)
+
+  const currentYear = new Date().getFullYear()
 
   const commits = screen.getByTestId('commits')
 
-  expect(commits.innerHTML).toBe('100 Total Commits')
+  expect(commits.innerHTML).toBe(`365&nbsp; Commits (${currentYear})`)
 })
 
 test('should render total pull request', () => {
-  render(<HomeGithubStats />)
+  render(<HomeGithubStats data={gitHubStats} />)
 
   const pullRequest = screen.getByTestId('pull-requests')
 
-  expect(pullRequest.innerHTML).toBe('5 Total Pull Requests')
+  expect(pullRequest.innerHTML).toBe('22 Pull Requests')
 })
 
 test('should render total issues', () => {
-  render(<HomeGithubStats />)
+  render(<HomeGithubStats data={gitHubStats} />)
 
   const issues = screen.getByTestId('issues')
 
-  expect(issues.innerHTML).toBe('10 Total Issues')
+  expect(issues.innerHTML).toBe('1&nbsp;Issues')
+})
+
+test('should render contributions', () => {
+  render(<HomeGithubStats data={gitHubStats} />)
+
+  const contributions = screen.getByTestId('contributions')
+
+  expect(contributions.innerHTML).toBe('4 Contributed to')
 })
