6c9cce9ca26b9c15cf346d1bc52808d8de2bf07f
rezaa <rezaageng5@gmail.com>
2023-08-13 19:23:07 +0700

    feat(home): :dizzy: add animations

--- src/components/home/stats/HomeGithubStats.tsx
+++ src/components/home/stats/HomeGithubStats.tsx
@@ -1,3 +1,5 @@
+'use client'
+
 import SvgGradientMotion from '@/components/SvgGradientMotion'
 import {
   GoRepo,
@@ -8,6 +10,16 @@ import {
   GoGitMerge
 } from 'react-icons/go'
 import { SiGithub } from 'react-icons/si'
+import {
+  type MotionStyle,
+  motion,
+  useScroll,
+  useAnimate,
+  useInView,
+  stagger
+} from 'framer-motion'
+import { useEffect, useRef } from 'react'
+import useSmooth from '@/hooks/useSmooth'
 
 interface Props {
   className?: string
@@ -15,6 +27,53 @@ interface Props {
 }
 
 const HomeGithubStats = ({ className = '', data }: Props): JSX.Element => {
+  // * hooks
+  const ref = useRef<HTMLDivElement>(null)
+
+  const [scope, animate] = useAnimate()
+
+  const isInview = useInView(scope, { margin: '-100px', once: true })
+
+  const { scrollYProgress } = useScroll({
+    target: ref,
+    offset: ['start end', 'end end']
+  })
+
+  // * animations
+  const wrapperScroll: MotionStyle = {
+    opacity: useSmooth(scrollYProgress, [0.2, 1], [0, 1])
+  }
+
+  const logoScroll: MotionStyle = {
+    opacity: useSmooth(scrollYProgress, [0.2, 0.7], [0, 1]),
+    x: useSmooth(scrollYProgress, [0.2, 0.7], [-100, 0])
+  }
+
+  useEffect(() => {
+    if (isInview) {
+      void animate(
+        'li',
+        {
+          opacity: [0, 1],
+          scale: [0.3, 1]
+        },
+        {
+          type: 'spring',
+          stiffness: 300,
+          damping: 30,
+          restDelta: 0.001,
+          delay: stagger(0.1, { startDelay: 0.15 })
+        }
+      )
+    } else {
+      void animate('li', {
+        opacity: 0,
+        scale: 0.3
+      })
+    }
+  }, [animate, isInview])
+
+  // * vars
   const { user } = data
 
   const stargazers = user.repositories.nodes
@@ -24,7 +83,9 @@ const HomeGithubStats = ({ className = '', data }: Props): JSX.Element => {
   const currentYear = new Date().getFullYear()
 
   return (
-    <div
+    <motion.div
+      ref={ref}
+      style={wrapperScroll}
       data-testid="bento-github"
       className={`${className} relative w-full overflow-hidden rounded-3xl`}
     >
@@ -34,12 +95,12 @@ const HomeGithubStats = ({ className = '', data }: Props): JSX.Element => {
           <h2 className="flex-1 bg-gradient-to-r from-accent-1 to-accent-3 bg-clip-text text-4xl font-semibold text-transparent  sm:text-3xl md:text-4xl">
             GitHub Stats
           </h2>
-          <div className="flex flex-1 justify-end">
+          <motion.div style={logoScroll} className="flex flex-1 justify-end">
             <SvgGradientMotion id="gh-gradient" from="#259D97" to="#1A6A87" />
             <SiGithub className="h-16 w-16 fill-[url(#gh-gradient)]" />
-          </div>
+          </motion.div>
         </div>
-        <ul className="z-10">
+        <ul ref={scope} className="z-10">
           <li className="flex items-center gap-2">
             <GoRepo />
             <span data-testid="public-repos">
@@ -81,7 +142,7 @@ const HomeGithubStats = ({ className = '', data }: Props): JSX.Element => {
           </li>
         </ul>
       </div>
-    </div>
+    </motion.div>
   )
 }
 
