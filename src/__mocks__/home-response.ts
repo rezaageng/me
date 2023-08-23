6560ba22ceed857f506cb87dbf6c864944332168
rezaa <rezaageng5@gmail.com>
2023-08-23 16:34:45 +0700

    test(mock): :clown_face: add short summary mock

--- src/__mocks__/home-response.ts
+++ src/__mocks__/home-response.ts
@@ -11,7 +11,10 @@ const homeRes: HomeResponse = {
       updatedAt: '2023-06-25T06:35:18.416Z',
       publishedAt: '2023-02-06T07:44:18.682Z',
       anotherSide: 'i like games, anime, manga, and manhwa',
-      summary: 'I like to code especially in front end dev heheheheh'
+      summary:
+        'Passionate Front-End Developer with 1+ years of experience creating visually stunning and user-friendly websites. Proficient in HTML, CSS, JavaScript, TypeScript, and modern front-end frameworks or libraries like React, Next.js, Tailwind, and Framer Motion. Adept at translating design mockups into responsive web experiences, optimizing performance, and ensuring cross-browser compatibility. A collaborative team player with a passion for staying up-to-date with the latest industry trends and technologies.',
+      shortSummary:
+        'Passionate Front-End Developer. Experience creating visually stunning and user-friendly websites. Proficient in modern front-end frameworks or libraries like React, Next.js, Tailwind CSS, and Framer Motion.'
     }
   },
   meta: {}
