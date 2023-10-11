dd34a33fa54b6c3c76831113d978e7d334204f6f
rezaa <rezaageng5@gmail.com>
2023-10-11 17:02:59 +0700

    chore(tailwind): :wrench: add tailwind typography

--- tailwind.config.js
+++ tailwind.config.js
@@ -43,5 +43,8 @@ module.exports = {
       }
     }
   },
-  plugins: [require('tailwind-scrollbar')({ nocompatible: true })]
+  plugins: [
+    require('tailwind-scrollbar')({ nocompatible: true }),
+    require('@tailwindcss/typography')
+  ]
 }
