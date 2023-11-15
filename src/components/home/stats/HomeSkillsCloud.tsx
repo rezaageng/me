df81985033a059596e7fa479a98f5ef321766408
rezaa <rezaageng5@gmail.com>
2023-11-15 13:37:28 +0700

    refactor(home): :recycle: use string icons instead of react-icons

--- src/components/home/stats/HomeSkillsCloud.tsx
+++ src/components/home/stats/HomeSkillsCloud.tsx
@@ -1,8 +1,5 @@
 import { Spherical, Vector3 } from 'three'
-import * as Icons from 'react-icons/si'
-import { HiLockClosed } from 'react-icons/hi'
 import { v4 as uuidv4 } from 'uuid'
-import { renderToString } from 'react-dom/server'
 import { useMemo } from 'react'
 import HomeSkillsIcon from './HomeSkillsIcon'
 import isFloat from '@/helpers/isFloat'
@@ -10,31 +7,32 @@ import isFloat from '@/helpers/isFloat'
 const HomeSkillsCloud = (): JSX.Element => {
   const icons = useMemo(() => {
     const temp: Array<[Vector3, string]> = []
-    const skills: JSX.Element[] = [
-      <Icons.SiJavascript key={uuidv4()} />,
-      <Icons.SiTypescript key={uuidv4()} />,
-      <Icons.SiDart key={uuidv4()} />,
-      <Icons.SiHtml5 key={uuidv4()} />,
-      <Icons.SiCss3 key={uuidv4()} />,
-      <Icons.SiNodedotjs key={uuidv4()} />,
-      <Icons.SiNpm key={uuidv4()} />,
-      <Icons.SiPnpm key={uuidv4()} />,
-      <Icons.SiReact key={uuidv4()} />,
-      <Icons.SiRedux key={uuidv4()} />,
-      <Icons.SiNextdotjs key={uuidv4()} />,
-      <Icons.SiFlutter key={uuidv4()} />,
-      <Icons.SiTailwindcss key={uuidv4()} />,
-      <Icons.SiStyledcomponents key={uuidv4()} />,
-      <Icons.SiFramer key={uuidv4()} />,
-      <Icons.SiThreedotjs key={uuidv4()} />,
-      <Icons.SiStrapi key={uuidv4()} />,
-      <Icons.SiJest key={uuidv4()} />,
-      <Icons.SiTestinglibrary key={uuidv4()} />,
-      <Icons.SiFigma key={uuidv4()} />,
-      <Icons.SiGit key={uuidv4()} />,
-      <Icons.SiGithub key={uuidv4()} />,
-      <Icons.SiVisualstudiocode key={uuidv4()} />,
-      <Icons.SiIntellijidea key={uuidv4()} />
+    const skills: string[] = [
+      'js',
+      'ts',
+      'dart',
+      'html5',
+      'css',
+      'md',
+      'vsc',
+      'git',
+      'github',
+      'intellij',
+      'nodejs',
+      'npm',
+      'react',
+      'nextjs',
+      'flutter',
+      'tailwind',
+      'styled',
+      'framer',
+      'strapi',
+      'three',
+      'reactquery',
+      'apollo',
+      'pnpm',
+      'jest',
+      'rtl'
     ]
 
     const lengthSqrt = Math.sqrt(skills.length)
@@ -53,8 +51,8 @@ const HomeSkillsCloud = (): JSX.Element => {
             spherical.set(16, phiSpan * i, thetaSpan * j)
           ),
           skills[currIndex] !== undefined
-            ? renderToString(skills[currIndex])
-            : renderToString(<HiLockClosed />)
+            ? `/assets/icons/${skills[currIndex]}.svg`
+            : '/assets/icons/lock.svg'
         ])
         currIndex++
       }
